3beb96fc5c13feacd5cb8b7bb1f1beb56b24ab90
7Ji <pugokushin@gmail.com>
2022-11-11 23:41:06 +0800

    create_fs: add label

--- build.sh
+++ build.sh
@@ -179,9 +179,9 @@ setup_loop() {
 create_fs() {
   echo " => Creating FS..."
   echo "  -> Creating FAT32 FS with UUID ${uuid_boot_mkfs} on ${loop_boot}"
-  sudo mkfs.vfat -F 32 -i "${uuid_boot_mkfs}" "${loop_boot}"
+  sudo mkfs.vfat -n 'ALARMBOOT' -F 32 -i "${uuid_boot_mkfs}" "${loop_boot}"
   echo "  -> Creating ext4 FS with UUID ${uuid_root} on ${loop_root}"
-  sudo mkfs.ext4 -m 0 -U "${uuid_root}" "${loop_root}"
+  sudo mkfs.ext4 -L 'ALARMROOT' -m 0 -U "${uuid_root}" "${loop_root}"
   echo " => Created FS"
 }
 
