c0ed278d85a2ba735c1611c9c3653a23e47ff5fb
7Ji <pugokushin@gmail.com>
2022-11-11 23:39:59 +0800

    prepare_uuid: fix empty boot uuid

--- build.sh
+++ build.sh
@@ -141,7 +141,7 @@ prepare_uuid() {
   echo " => Preparing UUID..."
   uuid_root=$(uuidgen)
   uuid_boot_mkfs=$(uuidgen)
-  uuid_boot_mkfs=${uuid_boot::8}
+  uuid_boot_mkfs=${uuid_boot_mkfs::8}
   uuid_boot_mkfs=${uuid_boot_mkfs^^}
   uuid_boot_specifier="${uuid_boot_mkfs::4}-${uuid_boot_mkfs:4}"
   echo "  -> UUID for root partition is ${uuid_root}"
