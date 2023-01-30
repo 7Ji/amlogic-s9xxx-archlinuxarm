d7f4bf42b149a56957a305e9bb171cddafedd984
7Ji <pugokushin@gmail.com>
2023-01-30 13:06:19 +0800

    build.sh: fix u-boot url

--- build.sh
+++ build.sh
@@ -102,7 +102,7 @@ prepare_uboot() {
     fi
     # A URL should look like this: 
     # https://github.com/ophub/amlogic-s9xxx-armbian/blob/main/build-armbian/u-boot/amlogic/overload/u-boot-s905x-s912.bin
-    ${wget} "${armbian_repo}/raw/${armbian_commit}/build-armbian/amlogic-u-boot/overload/u-boot-${uboot_name}.bin" -O "${uboot_file}"
+    ${wget} "${armbian_repo}/raw/${armbian_commit}/build-armbian/u-boot/amlogic/overload/u-boot-${uboot_name}.bin" -O "${uboot_file}"
     sha256sum_log=$(sha256sum "${uboot_file}") # This is written as a single command without piping to cut because I want it to fail it sha256sum fails
     sha256sum_actual="${sha256sum_log::64}"
     sha256sum_expected="${uboot_sha256sums[$i]}"
