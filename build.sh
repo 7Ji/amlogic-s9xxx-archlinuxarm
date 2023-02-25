9f3cccedc5082728c33c8d376327047678dbcaba
7Ji <pugokushin@gmail.com>
2023-02-25 14:24:53 +0800

    build.sh: change kernel commandline
     - added audit=0 to turn off audit framework
     - added apt_blkdevs=mmcblk2 to enable amlogic partition parsing on eMMC
     - removed other useless options

--- build.sh
+++ build.sh
@@ -135,7 +135,7 @@ populate_boot() {
   local conf_linux="vmlinuz-${kernel}"
   local conf_initrd="initramfs-${kernel}-fallback.uimg"
   local conf_fdt="dtbs/${kernel}/amlogic/PLEASE_SET_YOUR_DTB.dtb"
-  local conf_append="root=UUID=${uuid_root} rootflags=data=writeback rw rootfstype=ext4 console=ttyAML0,115200n8 console=tty0 no_console_suspend consoleblank=0 fsck.fix=yes fsck.repair=yes net.ifnames=0 cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory swapaccount=1"
+  local conf_append="root=UUID=${uuid_root} rw audit=0 apt_blkdevs=mmcblk2"
   local subst="
     s|%LINUX%|${conf_linux}|g
     s|%INITRD%|${conf_initrd}|g
